d6e3bc18826214d726b45d049881f913fb34e1c4
ChandlerG <chandler.giusti@gmail.com>
2016-02-03 15:08:57 -0800

    my first commit for lab1

---
+++ StudentView.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cgiusti
+ */
+public class StudentView {
+   public void printStudentDetails(String studentName, String studentRollNo){
+      System.out.println("Student: ");
+      System.out.println("Name: " + studentName);
+      System.out.println("Roll No: " + studentRollNo);
+   }
+}
